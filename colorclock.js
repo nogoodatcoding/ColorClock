77ce3def36072d8a1dc26835be014ec91e3f9390
James Khoury <James@jameskhoury.com>
2012-01-16 13:48:57 +1100

    Changed the way the font is increased/decreased so that it is specified on the #display div only and in em. Added Functionality to allow specifying the starting size of the font via the Query Parameters. Changed the Toggling of the time/hex values so they go up/down. Removed the #am/#pm code as it is now obsolete.

--- colorclock.js
+++ colorclock.js
@@ -3,36 +3,43 @@ $(document).ready(init);
 var lastHex = 0xffffff;
 var over60 = 256/60;
 var over24 = 256/24;
-var isAM = true;
+
+var startingFontSizeNum = 13;
+var currentFontSizeNum = startingFontSizeNum;
 
 function fontResizerInit() {
-  //credit: http://www.shopdev.co.uk/blog/text-resizing-with-jquery/
-  var originalFontSize = $('#timeval').css('font-size');
-    $(".resetFont").click(function(){
-    $('#timeval,#hexval,#am,#pm,#toggletime,#togglehex').css('font-size', originalFontSize);
+
+  var queryString = window.location.search;
+  var startSize;
+  if(queryString && (startSize = (/[&?]startSize=(\d*)/i).exec(queryString)))
+  {
+  currentFontSizeNum = startingFontSizeNum = parseInt(startSize[1], 10) || startingFontSizeNum;
+    $('#display').css('font-size', startingFontSizeNum + "em");
+  }
+  
+
+  $(".resetFont").click(function(){
+    $('#display').css('font-size', startingFontSizeNum + "em");
   });
+  
   // Increase Font Size
   $(".increaseFont").click(function(){
-    var currentFontSize = $('#timeval').css('font-size');
-    var currentFontSizeNum = parseFloat(currentFontSize, 10);
-    var newFontSize = currentFontSizeNum*1.1;
-    $('#timeval,#hexval,#am,#pm,#toggletime,#togglehex').css('font-size', newFontSize);
+    currentFontSizeNum += 0.25;
+    $('#display').css('font-size', currentFontSizeNum + "em");
     return false;
   });
   // Decrease Font Size
   $(".decreaseFont").click(function(){
-    var currentFontSize = $('#timeval').css('font-size');
-    var currentFontSizeNum = parseFloat(currentFontSize, 10);
-    var newFontSize = currentFontSizeNum*0.9;
-    $('#timeval,#hexval,#am,#pm,#toggletime,#togglehex').css('font-size', newFontSize);
+    currentFontSizeNum -= 0.25;
+    $('#display').css('font-size', currentFontSizeNum + "em");
     return false;
   });
 }
 
 function togglerInit() {
     $('#toggletime').click(function(){
-        $('#hexval').hide('slide', {direction: 'right'}, 'slow', function(){
-            $('#timeval').show('slide', {direction: 'right'}, 'slow');
+        $('#hexval:visible').hide('slide', {direction: 'down'}, 'slow', function(){
+            $('#timeval').show('slide', {direction: 'up'}, 'slow');
         });
         
         $(this).addClass('on');
@@ -40,8 +47,8 @@ function togglerInit() {
     });
     
     $('#togglehex').click(function(){
-        $('#timeval').hide('slide', {direction: 'left'}, 'slow', function(){
-            $('#hexval').show('slide', {direction: 'left'}, 'slow');
+        $('#timeval:visible').hide('slide', {direction: 'up'}, 'slow', function(){
+            $('#hexval').show('slide', {direction: 'down'}, 'slow');
         });
         
         $(this).addClass('on');
@@ -50,18 +57,6 @@ function togglerInit() {
 }
 
 function init() {
-    var d = new Date();
-    if (d.getHours() > 12 ) {
-        isAM = false;
-        $('#am').hide();
-        $('#pm').show();
-    }
-    else {
-        isAM = true;
-        $('#am').show();
-        $('#pm').hide();
-    }
-
     update();
     fontResizerInit();
     togglerInit();
@@ -73,34 +68,19 @@ function init() {
 
 function update() {
     var d = new Date();
-    checkAMPM(d);
 
     var hoursX = addLeadingZero(timeToHex(d.getHours(), over24));
     var minX = addLeadingZero(timeToHex(d.getMinutes(), over60));
     var secX = addLeadingZero(timeToHex(d.getSeconds(), over60));
     
     var hex = hoursX + minX + secX;
-    $('#timeval').html('<span>&nbsp;' + addLeadingZero("" + d.getHours()) + ":" + addLeadingZero("" + d.getMinutes()) + ":" + addLeadingZero("" + d.getSeconds()) + '</span>');
+    $('#timeval').text(addLeadingZero("" + d.getHours()) + ":" + addLeadingZero("" + d.getMinutes()) + ":" + addLeadingZero("" + d.getSeconds()));
     $('#hexval').text(hex);
     if(Math.abs(lastHex - parseInt('0x'+hex)) > 0x0A) {
         lastHex = parseInt('0x' + hex);
         $('body').animate({ backgroundColor: "#" +hex }, 'slow');
     }
     setTimeout(update, 1000);
-}
-
-function checkAMPM(d) {
-    if (d.getHours() >= 12 && isAM) {
-        isAM = false;
-        $('#am').hide();
-        $('#pm').show();
-    }
-    else if (d.getHours() < 12 && !isAM) {
-        console.log("hiding");
-        isAM = true;
-        $('#pm').hide();
-        $('#am').show();
-    }
 }
 
 function addLeadingZero(n) {
